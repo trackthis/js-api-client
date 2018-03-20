cee7fc94df1c85a4a9da2992a057d76b7a4d085c
Robin Bron <robin@finwo.nl>
2018-03-20 17:00:56 +0100

    Check if we're logged in already upon calling login

--- src/user/login/index.js
+++ src/user/login/index.js
@@ -1,4 +1,4 @@
-module.exports = function(scope) {
+module.exports = function (scope) {
 
   /**
    * Try to login through all available methods
@@ -7,35 +7,44 @@ module.exports = function(scope) {
    *
    * @return {Promise}
    */
-  return function(data) {
+  return function (data) {
     data = data || {};
     return scope
-      .checkTransport()
-      .then(scope.ensureManifest)
-      .then(scope.ensureSignatureConfig)
-      .then(function() {
-        return new Promise(function(resolve,reject) {cbq([
-          function(d,next) {
-            var user     = data.username || data.user || data.usr || data.account || data.acc || false,
-                username = ( user && user.username ) || ( user && user.name ) || user || undefined,
-                password = data.password || data.pass || data.passwd || data.pwd  || data.pw || undefined;
-            data.username = username;
-            data.password = password;
-            data.reject   = reject;
-            data.resolve  = function(response) {
-              scope.api.emit('login', response);
-              return resolve(response);
-            };
-            next(data);
-          },
+      .api.isLoggedIn()
+      .then(function (isLoggedIn) {
+        if (isLoggedIn) {
+          return Promise.resolve({});
+        }
+        return scope
+          .checkTransport()
+          .then(scope.ensureManifest)
+          .then(scope.ensureSignatureConfig)
+          .then(function () {
+            return new Promise(function (resolve, reject) {
+              cbq([
+                function (d, next) {
+                  var user      = data.username || data.user || data.usr || data.account || data.acc || false,
+                      username  = (user && user.username) || (user && user.name) || user || undefined,
+                      password  = data.password || data.pass || data.passwd || data.pwd || data.pw || undefined;
+                  data.username = username;
+                  data.password = password;
+                  data.reject   = reject;
+                  data.resolve  = function (response) {
+                    scope.api.emit('login', response);
+                    return resolve(response);
+                  };
+                  next(data);
+                },
 
-          require('./oauth/token')(scope),
-          require('./token/existing')(scope),
-          require('./token/existing-signed')(scope),
-          require('./username/signed')(scope),
-          require('./token/generated')(scope),
-          require('./oauth/auth')(scope),
-        ], reject.bind(undefined,'None of our supported authentication methods is supported by the server'), reject);});
+                require('./oauth/token')(scope),
+                require('./token/existing')(scope),
+                require('./token/existing-signed')(scope),
+                require('./username/signed')(scope),
+                require('./token/generated')(scope),
+                require('./oauth/auth')(scope),
+              ], reject.bind(undefined, 'None of our supported authentication methods is supported by the server'), reject);
+            });
+          });
       });
   };
 };
