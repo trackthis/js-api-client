26f599db1a6b9d414841705197d5d8526e7671ea
Robin Bron <robin@finwo.nl>
2018-03-26 18:07:11 +0200

    npm packages don't support symlinks

--- src/helper/cbq.js
+++ src/helper/cbq.js
@@ -1,1 +1,28 @@
-../../docs/assets/cbq.js
+(function (factory) {
+  var result = factory();
+
+  // Register to node if possible
+  /** global: define */
+  if (('undefined' !== typeof module) && ('undefined' !== typeof module.exports)) {
+    module.exports = result;
+  }
+
+  // Register to AMD or attach to the window
+  if (('function' === typeof define) && define.amd) {
+    define([], function () {
+      return result;
+    });
+  } else if ('undefined' !== typeof window) {
+    window.cbq = result;
+  }
+})(function() {
+  return function cbq(list,resolve,reject) {
+    var q = list.slice();
+    return (function next(data) {
+      var f = q.shift();
+      if (!f) { return ( 'function' === typeof resolve ) ? resolve(data) : data; }
+      setTimeout( function () { f( data, next, reject ); },0 );
+      return undefined;
+    })();
+  };
+});
